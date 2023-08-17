aedd685780b2aa9baa33b6d9322cef71c609a34d
marine <marinebeatrice@hotmail.fr>
2023-08-17 15:06:58 +0200

    handle error page on housing page

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -5,29 +5,29 @@ import { useState } from "react";
 
 const Carousel = ({ Images }) => {
 
-    const [currentPic, setCurrentPic] = useState(0)
+    const [currentIndex, setCurrentIndex] = useState(0)
     const length = Images.length
 
     const prev = () => {
-        setCurrentPic(currentPic === 0 ? length - 1 : currentPic - 1)
+        setCurrentIndex(currentIndex === 0 ? length - 1 : currentIndex - 1)
     }
 
     const next = () => {
-        setCurrentPic(currentPic === length - 1 ? 0 : currentPic + 1)
+        setCurrentIndex(currentIndex === length - 1 ? 0 : currentIndex + 1)
     }
     return (
         <section className="slides">
             {Images.map((image, index) => {
                 return (
-                    <div className={index === currentPic ? 'slides__container' : 'slides__hidden'} key={index}>
+                    <div className={index === currentIndex ? 'slides__container' : 'slides__hidden'} key={index}>
                         < img src={image} />
                     </div>
                 )
             })}
             {length > 1 && (
                 <div className="slides__controller">
-                    <a onClick={prev}><img src={arrowLeft} /></a>
-                    <a onClick={next}><img src={arrowRight} /></a>
+                    <a onClick={prev}><img src={arrowLeft} alt="Arrow to the left" /></a>
+                    <a onClick={next}><img src={arrowRight} alt="Arrow to the right" /></a>
                 </div>
             )}
         </section>
--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -1,10 +1,13 @@
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import Carousel from "../components/Carousel";
 import housing from "../datas/housing.json"
 
 function Housing() {
     const { id } = useParams()
     const logement = housing.find((logement) => logement.id === id)
+    if (!logement) {
+        return <Navigate to="/Error404" />
+    }
 
     return (
         <main>
