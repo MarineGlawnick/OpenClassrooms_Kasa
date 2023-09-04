b4488259aee7eee8387073447a6ecbc9ac9ae09c
marine <marinebeatrice@hotmail.fr>
2023-09-04 11:11:45 +0200

    remove bracket

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,9 +1,9 @@
 import "./_collapse.scss"
-import {useState} from "react";
+import { useState } from "react";
 import arrowDown from "../../assets/img/arrow_down.svg"
 import arrowUp from "../../assets/img/arrow_up.svg"
 
-function Collapse({content, title}) {
+function Collapse({ content, title }) {
     const [isOpen, setOpen] = useState(false)
     const setOpenCollapse = () => {
         setOpen(!isOpen)
@@ -12,18 +12,17 @@ function Collapse({content, title}) {
         <div className="collapse_container" onClick={setOpenCollapse}>
             <div className="collapse_container__title">
                 <h1>{title}</h1>
-                {isOpen ? <img src={arrowUp} alt="An arrow pointing up"/> :
-                    <img src={arrowDown} alt="An arrow pointing down"/>}
+                {isOpen ? <img src={arrowUp} alt="An arrow pointing up" /> :
+                    <img src={arrowDown} alt="An arrow pointing down" />}
             </div>
             {isOpen &&
                 <div className="collapse_container__content">
                     {Array.isArray(content) ?
                         (<ul className="collapse_container__content--list">
-                            {content.map((equipment, i) => (
-                                <li key={i}>{equipment}</li>))}
-                        < /ul>)
-                        : (<p>{content}</p>)
-                    }
+                            {content.map((equipment, i) =>
+                                (<li key={i}>{equipment}</li>))}
+                        </ul>)
+                        : (<p>{content}</p>)}
                 </div>
             }
         </div>
